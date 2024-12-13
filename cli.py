32761707d3046a145857fcd20027b79d1f2e6b4e
Panagiotis Sakagiannis <bagjohn0@gmail.com>
2024-12-13 13:46:42 +0200

    chore: nick help

--- cli.py
+++ cli.py
@@ -1,7 +1,25 @@
-#### How to structure tests  hints by Nick del Grosso
+'''
+Nicholas Del Grosso
+
+Hi Panos,
+
+Some extra explaining here (please forgive the long message):
+
+Attached is an updated version of the file, showing a better case of how to write the integration test (note that no "capsys" nor patching is needed in the integration test, making the test very easy to write and run, as it only deals with Python objects and no other systems.  
+
+"Don't patch your own code" is a good rule of thumb for helping write testable code; if we can make our code easier to test through mocks via dependency injection, then why not?   This approach also leads to the architectural practice of the Single Responsibility Principle in SOLID coding, with functions that have to do integration of systems (the cli() function in this example) being written to *only* do integration--nothing  else.  
+
+This has the final result of reducing the number of integration tests that are needed; a test of a mocked-out integration function is just a unit test.  This is a very good thing; integration tests imply coupled units, and coupled units are hard to maintain and extend, and can have non-obvious behaviors (that should be extra-tested as a result).  
+
+Anyway, hope that this demo was interesting!
+
+Best wishes,
+
+Nick
+'''
 
 from argparse import ArgumentParser
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 from pytest import mark
 
@@ -9,12 +27,12 @@ from pytest import mark
 def main(x):
     return x + 3
 
-def cli(args = None):
+def cli(args = None, mainfun=main, printer=print):
     parser = ArgumentParser(args=args)
     parser.add_argument('x')
     args = parser.parse_args()
     result = main(x=args.x)
-    print(result)
+    printer(result)
 
 
 
@@ -28,15 +46,20 @@ def test_cli_e2e(capsys):
     assert capsys.stdout == '6'
 
 
-# integration tests
-def test_cli(capsys):
-    with patch('__main__.main') as mock_main:
-        cli(args=['3'])
-        mock_main.assert_called_with(x=3)
+# integration tests no longer needed, because
+
+# unit test of an integration function (tests that integration happens correctly)
+def test_cli():
+    mock_main = Mock()
+    mock_main.return_value(6)
+    mock_print = Mock()
+    cli(args=['3'], mainfun=mock_main, printer=mock_print)
+    mock_main.assert_called_with(x=3)
+    mock_print.assert_called_once_with(6)
 
 
 
-# unit tests
+# unit test
 def test_main():
     result = main(3)
     assert result == 6
